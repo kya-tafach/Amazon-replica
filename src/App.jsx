da856a45fad03432377dbb01701e4737bdd728f9
kya-tafach <gmekb41@gmail.com>
2024-11-03 04:26:28 -0500

    removed carousel from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useContext,useState,useEffect } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
-import CarouselEffect from './Components/Carousel/CarouselEffect'
 import { BiCategory } from 'react-icons/bi'
 import Category from './Components/Category/Category'
 import Product from './Components/Product/Product'
