80b954314f40209ac41f9e81000f37d3d1eabc34
kya-tafach <gmekb41@gmail.com>
2024-11-03 04:37:59 -0500

    updated carousel import

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -4,6 +4,7 @@ import Layout from '../../Components/Layout/Layout'
 import Carousel  from '../../Components/Carousel/CarouselEffect'
 import Category from '../../Components/Category/Category'
 import Product from '../../Components/Product/Product'
+
 function Landing() {
   return (
     <Layout>
