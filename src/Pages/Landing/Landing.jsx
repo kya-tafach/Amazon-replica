f23dd7455887f423dfc5a53e06e8306863d5207a
kya-tafach <gmekb41@gmail.com>
2024-11-03 04:59:01 -0500

    carousel effect import change 3

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Layout from '../../Components/Layout/Layout'
 import Category from '../../Components/Category/Category'
 import Product from '../../Components/Product/Product'
-import CarouselEffect from "../../Components/Carousel/CarouselEffect"
+import CarouselSlider from "../../Components/Carousel/CarouselEffect"
 
 function Landing() {
   return (
