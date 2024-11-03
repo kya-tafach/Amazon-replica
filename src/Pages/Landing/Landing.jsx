840da96d407526a667157ec239a78c1aa83ec2b9
kya-tafach <gmekb41@gmail.com>
2024-11-03 04:47:39 -0500

    updated carousel import with .jsx

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Layout from '../../Components/Layout/Layout'
 import Category from '../../Components/Category/Category'
-import Carousel from "../../Components/Carousel/CarouselEffect"
+import Carousel from "../../Components/Carousel/CarouselEffect.jsx"
 import Product from '../../Components/Product/Product'
 
 function Landing() {
